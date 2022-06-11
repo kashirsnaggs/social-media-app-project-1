20b74788329d40edfae1e9af521bdd3cf9cbb4a9
Kashir Snaggs <snaggs.kashir@gmail.com>
2022-06-11 16:25:59 -0400

    Day1

---
+++ src/Data/PostsData.js
@@ -0,0 +1,28 @@
+import postPic1 from '../img/postpic1.jpg'
+import postPic2 from '../img/postpic2.jpg'
+import postPic3 from '../img/postpic3.jpg'
+
+
+export const PostsData = [
+  {
+   img: postPic1,
+   name: 'Naruto',
+   desc: "Naruto being annoying! #Naruto",
+   likes: "700k",
+   liked: true
+  },
+  {
+    img: postPic2,
+    name: 'Sasuke',
+    desc: "Sasuke thinking mad ideas",
+    likes: "590k",
+    liked: false
+   },
+   {
+    img: postPic3,
+    name: 'Pain',
+    desc: "me feeling powerful after my mom hits dont hurt",
+    likes: "1.2m",
+    liked: true
+   },
+];
